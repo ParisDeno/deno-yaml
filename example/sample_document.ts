85044be0cd405f2607ff5ab652d82e190b0b8789
Lilian Saget-Lethias <lilian.sagetlethias@gmail.com>
2019-06-30 23:25:09 +0200

    :arrow_up: Update node deps

--- example/sample_document.ts
+++ example/sample_document.ts
@@ -7,9 +7,10 @@ const { readFileSync, cwd, inspect } = Deno;
 
     const document = new TextDecoder().decode(yml);
     const obj = parseSync(document);
+    console.log(obj);
 
     let i = 0;
-    for (const o of obj) {
+    for (const o of Object.values(obj)) {
         console.log(`======${i}`);
         for (const [key, value] of Object.entries(o)) {
             console.log(key, value);
